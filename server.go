9d69d342511d3e6242ac15eb8119842de2aea2b0
Fisherman August <fshrmn.august@gmail.com>
2025-07-27 11:13:15 +0300

    POST added

--- server.go
+++ server.go
@@ -29,11 +29,22 @@ func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
+func CreateUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	user.Id = fmt.Sprintf("%d", len(users)+1)
+	users = append(users, user)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(Response{Message: "New user appended"})
+
+}
+
 func main() {
 	users = append(users, User{Id: "1", Name: "Vasya", Email: "vasya@mail.ru"})
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexHandler).Methods("GET")
 	r.HandleFunc("/users", GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users", CreateUsersHandler).Methods("POST")
 	fmt.Println("server staring...")
 	http.ListenAndServe(":8080", r)
 }
