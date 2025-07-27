f369fefb98b14a5bc378d7ab8c4f14842cc6a9d7
Fisherman August <fshrmn.august@gmail.com>
2025-07-27 10:55:55 +0300

    get method

--- server.go
+++ server.go
@@ -8,6 +8,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type User struct {
+	Id    string `json: "id"`
+	Name  string `json: "name"`
+	Email string `json: "email"`
+}
+
+var users = []User{}
+
 type Response struct {
 	Message string `json:"message"`
 }
@@ -16,10 +24,16 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(Response{Message: "Api is working now"})
 }
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
 
 func main() {
+	users = append(users, User{Id: "1", Name: "Vasya", Email: "vasya@mail.ru"})
 	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/", IndexHandler).Methods("GET")
+	r.HandleFunc("/users", GetUsersHandler).Methods("GET")
 	fmt.Println("server staring...")
 	http.ListenAndServe(":8080", r)
 }
