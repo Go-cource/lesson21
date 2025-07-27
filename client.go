5c6addfd6df49b0c9bae8ea3c491375088228090
Fisherman August <fshrmn.august@gmail.com>
2025-07-27 12:43:12 +0300

    PUT request client

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 // Get request
@@ -23,7 +24,7 @@ func createGetRequest() {
 	fmt.Println("Sever answer: ", string(body))
 }
 
-func main() {
+func createPostRequest() {
 	user := []byte(`{"id":"0", "name": "Dima", "email": "dima@gmail.com"}`)
 	resp, err := http.Post("http://127.0.0.1:8080/users", "application/json", bytes.NewBuffer(user))
 	if err != nil {
@@ -38,4 +39,31 @@ func main() {
 	}
 
 	fmt.Println("Server answered: ", string(body))
+}
+
+func main() {
+	client := &http.Client{
+		Timeout: 4 * time.Second,
+	}
+	user := []byte(`{"id":"4", "name": "Dmitry", "email": "Dmitry@gmail.com"}`)
+	req, err := http.NewRequest("PUT", "http://127.0.0.1:8080/users/4", bytes.NewBuffer(user))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "HelloFromCookie")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("ReadAll error", err)
+		return
+	}
+	fmt.Println("Server answered: ", string(body))
+
 }
